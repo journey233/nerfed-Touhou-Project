1f373ab6e60b3f45cc9e5ace61f197cbcc0b6b9f
journey233 <1137502599@qq.com>
2024-10-13 16:52:06 +0800

    2.1

--- AircraftBattle/base_plane_class.h
+++ AircraftBattle/base_plane_class.h
@@ -3,7 +3,7 @@
 
 #include <QGraphicsPixmapItem>
 #include <QList>
-// #include "bullet.h"
+#include "bullet.h"
 
 #define SELF true
 #define ENEMY false
@@ -16,7 +16,7 @@ private:
     int _speed; // 移动速度
     // 自机可能还需要添加是否处于无敌状态的变量
     bool _camp; // 机体的阵营 自机->SELF，敌机->ENEMY
-    // QList<Bullet> bullet_list; // 每个机体发射的子弹的链表
+    QList<Bullet> bullet_list; // 每个机体发射的子弹的链表
 public:
     base_plane_class(const QPixmap &p, int l, int s, bool c, QPointF pos, QSize scale, QGraphicsPixmapItem *parent = nullptr)
         :_life(l), _speed(s), _camp(c)
@@ -34,8 +34,8 @@ public:
     }
     void attack(int type, int dx, int dy) // 机体向（dx, dy）方向发射一颗子弹
     {
-        // Bullet b = new Bullet(type, dx, dy);
-        // bullet_list.append(b);
+        Bullet b(type, dx, dy);
+        bullet_list.append(b);
     }
     void be_attacked() // 机体受击
     {
